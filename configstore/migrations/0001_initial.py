8bc82f439e0bf825bf7f1b2dea96e00adaf919cd
James G. Reynolds <eire1130@gmail.com>
2016-10-21 15:53:44 -0400

    added django 1.7 migrations

--- configstore/migrations/0001_initial.py
+++ configstore/migrations/0001_initial.py
@@ -1,48 +1,27 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Configuration'
-        db.create_table('configstore_configuration', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('key', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
-            ('_data', self.gf('django.db.models.fields.TextField')(db_column='data')),
-        ))
-        db.send_create_signal('configstore', ['Configuration'])
+    dependencies = [
+        ('sites', '0001_initial'),
+    ]
 
-        # Adding unique constraint on 'Configuration', fields ['key', 'site']
-        db.create_unique('configstore_configuration', ['key', 'site_id'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'Configuration', fields ['key', 'site']
-        db.delete_unique('configstore_configuration', ['key', 'site_id'])
-
-        # Deleting model 'Configuration'
-        db.delete_table('configstore_configuration')
-
-
-    models = {
-        'configstore.configuration': {
-            'Meta': {'unique_together': "[('key', 'site')]", 'object_name': 'Configuration'},
-            '_data': ('django.db.models.fields.TextField', [], {'db_column': "'data'"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'key': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"})
-        },
-        'sites.site': {
-            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
-            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        }
-    }
-
-    complete_apps = ['configstore']
+    operations = [
+        migrations.CreateModel(
+            name='Configuration',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(max_length=50)),
+                ('data', models.TextField(db_column=b'data')),
+                ('site', models.ForeignKey(to='sites.Site')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='configuration',
+            unique_together=set([('key', 'site')]),
+        ),
+    ]
