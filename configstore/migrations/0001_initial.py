2130a293ae318c563097841b0ec0ae9372d2371b
James G. Reynolds <eire1130@gmail.com>
2016-11-29 15:24:48 -0500

    removed b from string for python 3 compat

--- configstore/migrations/0001_initial.py
+++ configstore/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('key', models.CharField(max_length=50)),
-                ('data', models.TextField(db_column=b'data')),
+                ('data', models.TextField(db_column='data')),
                 ('site', models.ForeignKey(to='sites.Site')),
             ],
         ),
