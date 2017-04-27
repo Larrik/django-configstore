f31d5c3d928447438b2ed6b49aae91fe3e6c255b
James G. Reynolds <eire1130@gmail.com>
2017-04-27 15:06:00 -0400

    added app config

--- configstore/__init__.py
+++ configstore/__init__.py
@@ -1,3 +1,1 @@
-from configstore.configs import register, get_config, ConfigurationInstance
-from configstore.forms import ConfigurationForm
-import configstore.listeners
+default_app_config = 'configstore.apps.ConfigstoreConfig'
---
+++ configstore/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class ConfigstoreConfig(AppConfig):
+    name = 'configstore'
+    verbose_name = "configstore"
